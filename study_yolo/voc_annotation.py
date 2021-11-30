8b755bf6740d02c68c3db21d885d737c1ec8c3eb
HyeJu99 <sinhye102@gmail.com>
2021-12-01 00:30:33 +0900

    update: reshape error trying, but there is reshape error 43095 values, but required 4563(39*39*3)

--- study_yolo/voc_annotation.py
+++ study_yolo/voc_annotation.py
@@ -1,34 +1,44 @@
 import xml.etree.ElementTree as ET
 from os import getcwd
+import glob
 
-sets=[('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
+classes = ["deer", "beam", "gcooter", "others"]
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+def convert_annotation(annotation_voc, train_all_file, class_id):
+    txt_file = open(f'{annotation_voc}', 'r')
+    box_list = txt_file.read().split('\n')[0].split(' ')[1:]
 
+    box = ''
+    for b in box_list:
+        if box == '':
+            box = b
+        else:
+            box = box + ',' + b
+        #print(box)
+    train_all_file.write(" "+ box + ',' + str(class_id))
 
-def convert_annotation(year, image_id, list_file):
-    in_file = open('VOCdevkit/VOC%s/Annotations/%s.xml'%(year, image_id))
-    tree=ET.parse(in_file)
-    root = tree.getroot()
+train_all_file = open('./data/light/train_all.txt', 'w')
 
-    for obj in root.iter('object'):
-        difficult = obj.find('difficult').text
-        cls = obj.find('name').text
-        if cls not in classes or int(difficult)==1:
-            continue
-        cls_id = classes.index(cls)
-        xmlbox = obj.find('bndbox')
-        b = (int(xmlbox.find('xmin').text), int(xmlbox.find('ymin').text), int(xmlbox.find('xmax').text), int(xmlbox.find('ymax').text))
-        list_file.write(" " + ",".join([str(a) for a in b]) + ',' + str(cls_id))
-
-wd = getcwd()
-
-for year, image_set in sets:
-    image_ids = open('VOCdevkit/VOC%s/ImageSets/Main/%s.txt'%(year, image_set)).read().strip().split()
-    list_file = open('%s_%s.txt'%(year, image_set), 'w')
-    for image_id in image_ids:
-        list_file.write('%s/VOCdevkit/VOC%s/JPEGImages/%s.jpg'%(wd, year, image_id))
-        convert_annotation(year, image_id, list_file)
-        list_file.write('\n')
-    list_file.close()
-
+# Get annotations_voc list
+for className in classes:
+    annotations_voc = glob.glob(f'./data/light/image/train/v*.txt')
+    for annotation_voc in annotations_voc:
+        #print(annotation_voc)
+        #print(annotation_voc.split('/')[-1].split('\\')[-1].split('.')[0])
+        
+        txt_file = open(f'{annotation_voc}', 'r')
+        class_id = txt_file.read().split('\n')[0].split(' ')[0]
+        
+        ''' class 여러개일 경우
+        class_id = []
+        for line in txt_file.read().split('\n'):
+            if line != '':
+                class_id.append(line.split(' ')[0])
+        print(class_id)
+        '''
+        
+        image_id = annotation_voc.split('/')[-1].split('\\')[-1].split('.')[0]+'.JPG'
+        train_all_file.write(f'./data/light/image/train/{class_id}/{image_id}')
+        convert_annotation(annotation_voc, train_all_file, class_id)
+        train_all_file.write('\n')
+train_all_file.close()
